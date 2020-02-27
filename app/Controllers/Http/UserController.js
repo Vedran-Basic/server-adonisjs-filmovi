bd742bd41335ad233e8256a7fbcf2db8ac1d7095
Vedran-Basic <vedran.basic98@gmail.com>
2020-02-27 12:19:20 +0100

    need to add some modules: update them

--- app/Controllers/Http/UserController.js
+++ app/Controllers/Http/UserController.js
@@ -1,20 +1,36 @@
 'use strict'
 
 const User = use('App/Models/User')
+const { validate } = use('Validator')
 
 
 
 class UserController {
-async register({ request, response }) {
-    const { email, password } = request.all()
-    
-    const user = await User.create({
-      email,
-      password,
-      username: email 
-    })
+  async register({ request, session, response }) {
+    const rules = {
+      email: 'required|email|unique:users,email',
+      password: 'required'
+    }
+    const validation = await validate(request.all(), rules)
 
-    return user
+    if (validation.fails()) {
+      session
+        .withErrors(validation.messages())
+        .flashExcept(['password'])
+      return response.redirect('back')
+    }
+    else {
+      const { email, password } = request.all()
+
+      const user = await User.create({
+        email,
+        password,
+        username: email
+      })
+
+      return user
+    }
+    
 
 
   }
